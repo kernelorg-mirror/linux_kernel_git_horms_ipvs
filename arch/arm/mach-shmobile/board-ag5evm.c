ad95fc97a59a01cc576713e5ad419738420eebfa
Teppei Kamijou <teppei.kamijou.yb@renesas.com>
2010-12-21 16:06:20 +0900

    ARM: PL310 power mode: Dynamic clock gating (experimental)
    Redmine: refs #16206
    
    When the dynamic high-level clock-gating feature is enabled, the cache
    controller stops its clock when it is idle.  It does not stop the clock
    immediately when it is idle, but after it counts several clock cycles.
    
    *** TODO ***
    
    * Implement RTL release number check -- Older revision of PL310 cache
      controller does not have the Power Control Register.
    
    Signed-off-by: Teppei Kamijou <teppei.kamijou.yb@renesas.com>
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- arch/arm/mach-shmobile/board-ag5evm.c
+++ arch/arm/mach-shmobile/board-ag5evm.c
@@ -812,6 +812,8 @@ static void __init ag5evm_init(void)
 	gpio_set_value(GPIO_PORT217, 1);
 
 #ifdef CONFIG_CACHE_L2X0
+	/* Enable Dynamic clock gating */
+	__raw_writel(0x00000002, __io(0xf0100000) + L2X0_POWER_CTRL);
 	/* Early BRESP enable, Shared attribute override enable, 64K*8way */
 	l2x0_init(__io(0xf0100000), 0x40460000, 0x82000fff);
 #endif
