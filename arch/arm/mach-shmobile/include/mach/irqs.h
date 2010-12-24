b5002d50981b6153d073ab83374b25798720e1af
Takashi Yoshii <takashi.yoshii.zj@renesas.com>
2010-12-24 11:10:00 +0900

    ARM: mach-shmobile: sh73a0: Introduce soft_irq
    This is to be used to handle software produced irqs such as decoded irq
    from PINT and the others.
    This provides only a hint for irq number assignment.
    Need to be modified accoding to future expansion of irqs usage.
    
    Signed-off-by: Takashi Yoshii <takashi.yoshii.zj@renesas.com>

--- arch/arm/mach-shmobile/include/mach/irqs.h
+++ arch/arm/mach-shmobile/include/mach/irqs.h
@@ -16,4 +16,9 @@
 #define INTCS_VECT(n, vect)	INTC_VECT((n), INTCS_VECT_BASE + (vect))
 #define intcs_evt2irq(evt)	evt2irq(INTCS_VECT_BASE + (evt))
 
+/* Soft IRQ */
+/* This must be bigger than last INTCS and less than NR_IRQS */
+#define SOFT_IRQ_BASE		(NR_IRQS - 16)
+#define soft_irq(nr)		((nr) + SOFT_IRQ_BASE)
+
 #endif /* __ASM_MACH_IRQS_H */
