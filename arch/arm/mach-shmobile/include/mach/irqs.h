fa57e6625a1c41dccb94387b89b8bec400cc4ada
Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>
2011-01-31 21:27:14 +0900

    ARM: shmobile: intc-sh73a0: Put the software IRQs at the end of the INTCS
    Redmine: refs #16570
    
    To keep the status of use of irq_desc[] resources compact, we'd like to
    have the software IRQs at the end of INTCS IRQs, even though NR_IRQS is
    now increased up to 1024.
    
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- arch/arm/mach-shmobile/include/mach/irqs.h
+++ arch/arm/mach-shmobile/include/mach/irqs.h
@@ -16,9 +16,8 @@
 #define INTCS_VECT(n, vect)	INTC_VECT((n), INTCS_VECT_BASE + (vect))
 #define intcs_evt2irq(evt)	evt2irq(INTCS_VECT_BASE + (evt))
 
-/* Soft IRQ */
-/* This must be bigger than last INTCS and less than NR_IRQS */
-#define SOFT_IRQ_BASE		(NR_IRQS - 16)
+/* Soft IRQ - allocate unused IRQ resources at the end of the INTCS IRQs */
+#define SOFT_IRQ_BASE		(512 - 16)
 #define soft_irq(nr)		((nr) + SOFT_IRQ_BASE)
 
 /* PINT (for sh73a0) */
