bf2c94c03541582126ec28598e1082ebbf8f16d7
Teppei Kamijou <teppei.kamijou.yb@renesas.com>
2011-03-23 16:14:10 +0900

    i2c-sh_mobile: Fix up ICCH/ICCL calculation
    Redmine: refs #17689, #17747
    
    On recent SH-Mobile device, the clock source to IIC module has been
    changed from SUB clock to HP clock, and at the same time SCL clock
    calculation formula is revised.  Fix the code accordingly.
    
    Signed-off-by: Teppei Kamijou <teppei.kamijou.yb@renesas.com>
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>

--- drivers/i2c/busses/i2c-sh_mobile.c
+++ drivers/i2c/busses/i2c-sh_mobile.c
@@ -180,7 +180,11 @@ static void activate_ch(struct sh_mobile_i2c_data *pd)
 	 * We also round off the result.
 	 */
 	num = i2c_clk * 5;
+#ifdef CONFIG_ARCH_SH73A0
+	denom = NORMAL_SPEED * 9 * 2;
+#else
 	denom = NORMAL_SPEED * 9;
+#endif
 	tmp = num * 10 / denom;
 	if (tmp % 10 >= 5)
 		pd->iccl = (u_int8_t)((num/denom) + 1);
